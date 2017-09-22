cafc46a8ce423766a0ba2e13b472a16246a630dd
yuezhiyong <yuehzhiyong@ktjr.com>
2017-09-22 11:01:58 +0800

    first commit

---
+++ src/main/java/com/ktjr/ktwd/groovy/ShareGroovyApplication.java
@@ -0,0 +1,12 @@
+package com.ktjr.ktwd.groovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareGroovyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareGroovyApplication.class, args);
+	}
+}
