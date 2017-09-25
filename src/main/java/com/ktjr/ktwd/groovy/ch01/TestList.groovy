da494d834ef5a00b83e7acbee86df3e4e256ab4c
yuezhiyong <yuehzhiyong@ktjr.com>
2017-09-25 11:27:39 +0800

    list的毁三观操作

--- src/main/java/com/ktjr/ktwd/groovy/ch01/TestList.groovy
+++ src/main/java/com/ktjr/ktwd/groovy/ch01/TestList.groovy
@@ -6,7 +6,7 @@ package com.ktjr.ktwd.groovy.ch01
 class TestList {
 
     static void main(args){
-        test6()
+        test13()
     }
 
     static void test1(){
@@ -72,4 +72,76 @@ class TestList {
     static void test6(){
         assert [1,[2,3]].flatten() == [1,2,3]
     }
+
+    /**
+     * List的元素的类型可以是任意类型 包括其他的嵌套类型
+     * 这可以用来实现列表的列表：即java中的多位数组
+     * 列表也可以像栈操作一样，支持pop和push 就如同使用左移 << 操作符
+     */
+    static void test7(){
+
+        def myList = [1,2,3]
+        myList << 4 << 5
+        assert myList == [1,2,3,4,5]
+
+        myList >> 5
+        assert myList == [1,2,3,4]
+    }
+
+    static void test8(){
+        def x = [1,1,1]
+        assert [1] == new HashSet(x).toList()
+        assert [1] == x.unique()
+    }
+
+    static void test9(){
+        def x = [1, null, 1]
+        assert [1,1] == x.findAll {
+            it != null
+        }
+        assert [1,1] == x.grep{it}
+    }
+
+    static void test10(){
+        def list = [1,2,'r','x']
+        def result = list.collect{it *2}
+        println result
+        def res = result.findAll{
+            it ->
+                !(it instanceof Integer)
+        }
+        // or write like this:
+        println res.grep{!(it instanceof String)}
+        println res
+    }
+
+    static void test11(){
+        def lists = [1,3]
+        def res = lists.every{
+            it ->
+                it > 0
+        }
+        println res
+    }
+
+    static void test12(){
+        def list = [2,34,56]
+        def count = list.inject(10){
+            result, it ->
+                result + it
+        }
+        println count
+    }
+    /**
+     * 还有一个集合方法，对于字符串特别好用
+     *
+     */
+    static void test13(){
+
+        def list = ['xxxsdf','rervf','gtg']
+        def result = list.join("---")
+        println result
+
+    }
+
 }
